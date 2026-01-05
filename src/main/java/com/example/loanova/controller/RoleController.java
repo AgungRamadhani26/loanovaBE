f330a323144194eb49abe9fca6b72af8df9a077b
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2026-01-05 10:27:54 +0700

    tes spotless

--- src/main/java/com/example/loanova/controller/RoleController.java
+++ src/main/java/com/example/loanova/controller/RoleController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import lombok.Setter;
 
 @RestController
 @RequestMapping("/api/roles")
